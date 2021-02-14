621e9522be8752cd9c6276fac53e8bbfd1563d17
aryan-abdol <aryan.abdolhosseini@gmail.com>
2021-02-14 16:47:32 -0500

    Credits page completed

--- SeniorProj/Settings/Credits.swift
+++ SeniorProj/Settings/Credits.swift
@@ -7,24 +7,70 @@ class Credits: FormViewController {
          self.tableViewStyle = .insetGrouped //We'll uncomment this later.
          super.viewDidLoad()
          
-       
-      
+        
+        
         
         
          form +++ Section("App Developers")
             
-            <<< LabelRow("password") {
-                $0.title = "John"
-                $0.value = "Developer"
+            <<< LabelRow() {
+                $0.title = "Mathew Yaldo"
+                $0.value = "IOS & Android Developer"
+                
+            }
+         
+            <<< LabelRow() {
+                $0.title = "Aryan Abdolhosseini"
+                $0.value = "IOS Developer"
                 
             }
         
-            <<< TextRow("confirm") {
-                $0.title = "Confirm Password"
-                $0.placeholder = "Confirm Password"
+            <<< LabelRow() {
+                $0.title = "Adrian Berishaj"
+                $0.value = "IOS Developer"
+                
             }
         
+            <<< LabelRow() {
+                $0.title = "Yousif Hanani"
+                $0.value = "Android Developer"
+                
+            }
+        
+            <<< LabelRow() {
+                $0.title = "Tin Liu"
+                $0.value = "Android Developer"
+                
+            }
            
+        
+        
+        form +++ Section("Libraries and Frameworks")
+           
+           <<< LabelRow() {
+               $0.title = "NFCReaderWriter"
+               
+           }
+        
+           <<< LabelRow() {
+               $0.title = "Eureka"
+               
+           }
+       
+           <<< LabelRow() {
+               $0.title = "CryptoSwift"
+               
+           }
+       
+           <<< LabelRow() {
+               $0.title = "CRDCrypt"
+               
+           }
+       
+           <<< LabelRow() {
+               $0.title = "BiometricAuthentication"
+               
+           }
 
         
         
